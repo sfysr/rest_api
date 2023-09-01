76f2b5218b297e951939d35c127b04421490ae34
sfysr <muhammetsefayasar@gmail.com>
2023-09-01 17:19:14 +0300

    Repo init

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const uri = 'mongodb+srv://muhammetsefayasar:MSYasardatabasepass@cluster0.qnd5thx.mongodb.net/';
+const app = express();
+const port = process.env.PORT || 3000;
+
+// MongoDB bağlantısı
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Veritabanı şema tanımı
+const dataItemSchema = new mongoose.Schema({
+  label: String,
+  key: String,
+  value: String,
+  lastUpdate: Date,
+});
+
+const DataItem = mongoose.model('DataItem', dataItemSchema);
+
+app.use(express.json());
+
+// CREATE: Yeni bir veri eklemek için
+app.post('/api/dataitem', (req, res) => {
+  const newDataItem = new DataItem({
+    label: req.body.label,
+    key: req.body.key,
+    value: req.body.value,
+    lastUpdate: new Date(),
+  });
+
+  newDataItem
+    .save()
+    .then((dataItem) => {
+      res.json(dataItem);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+// READ: Tüm verileri getirmek için
+app.get('/api/dataitem', (req, res) => {
+  DataItem.find()
+    .then((dataItems) => {
+      res.json(dataItems);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+// UPDATE: Veri güncellemek için
+app.put('/api/dataitem/:id', (req, res) => {
+  DataItem.findByIdAndUpdate(
+    req.params.id,
+    {
+      label: req.body.label,
+      key: req.body.key,
+      value: req.body.value,
+      lastUpdate: new Date(),
+    },
+    { new: true }
+  )
+    .then((dataItem) => {
+      res.json(dataItem);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+// DELETE: Veri silmek için
+app.delete('/api/dataitem/:id', (req, res) => {
+  DataItem.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.json({ message: 'Veri silindi..' });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+  
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
